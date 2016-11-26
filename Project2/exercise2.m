a2ab57295369993ffd2c621355550bc797d01a05
Morten Ledum <mortele@student.matnat.uio.no>
2016-11-26 20:48:37 +0100

    Updating report 2.

--- Project2/exercise2.m
+++ Project2/exercise2.m
@@ -13,7 +13,7 @@ plotting    = true;
 
 %% Functions
 % Simple delta function.
-delta   = @(a,b)            a == b;
+delta   = @(a,b) a == b;
 
 % One body operator expectation value, <p1q1|H0|p2q2>.
 H0      = @(p1,q1,p2,q2)    2*(p1-1)*delta(p1,p2)*delta(q1,q2) + ...
@@ -28,14 +28,14 @@ H       = @(p1,q1,p2,q2,g) H0(p1,q1,p2,q2) + V(p1,q1,p2,q2,g);
 
 % Map two indices (a state |ij>) onto a single index.
 map     = @(I) [1,2]*(I==1) + [1,3]*(I==2) + [1,4]*(I==3) + ...
-               [2,3]*(I==4) + [2,4]*(I==5) + [3,4]*(I==6);
+               [2,4]*(I==4) + [2,3]*(I==5) + [3,4]*(I==6);
 
 % The constant prefactor of the operator R (1/(e0-ej)), in terms of a
 % single index (as mapped above).
 gamma   = @(I) 1./(H0(1,2,1,2) - (I==2) * H0(1,3,1,3) ...
                                - (I==3) * H0(1,4,1,4) ...
-                               - (I==4) * H0(2,3,2,3) ...
-                               - (I==5) * H0(2,4,2,4) ...
+                               - (I==4) * H0(2,4,2,4) ...
+                               - (I==5) * H0(2,3,2,3) ...
                                - (I==6) * H0(3,4,3,4));
 
 %% Set up and diagonalize FCI matrix
@@ -43,7 +43,7 @@ FCI     = zeros(basisSize,   basisSize);
 CID     = zeros(basisSize-1, basisSize-1);
 G       = linspace(-1,1,n); 
 E_FCI   = zeros(n,basisSize);
-E_CID   = zeros(n,basisSize);
+E_CID   = zeros(n,basisSize-1);
 E_RS3   = zeros(n,1);
 f_FCI   = zeros(n,1);
 f_CID   = zeros(n,1);
@@ -52,13 +52,9 @@ for k=1:n
     g = G(k);
     for i=1:basisSize
         for j=1:basisSize
-            p1q1        = map(i);
-            p2q2        = map(j);
-            p1          = p1q1(1);
-            q1          = p1q1(2);
-            p2          = p2q2(1);
-            q2          = p2q2(2);
-            FCI(i,j)    = H(p1,q1,p2,q2,g);
+            p1q1     = map(i); p1 = p1q1(1); q1 = p1q1(2);
+            p2q2     = map(j); p2 = p2q2(1); q2 = p2q2(2);
+            FCI(i,j) = H(p1,q1,p2,q2,g);
         end
     end
     
@@ -77,13 +73,13 @@ for k=1:n
     % Diagonalize the FCI and the CID matrices, extract the ground state
     % energy and the ground state probability, f.
     [V_FCI, e_FCI]  = eig(FCI);
-    [V_CID, e_CID]  = eig(FCI);
+    [V_CID, e_CID]  = eig(CID);
     e_FCI           = diag(e_FCI);
     e_CID           = diag(e_CID);
     E_FCI(k,:)      = e_FCI;
     E_CID(k,:)      = e_CID;
-    GS_FCI          = zeros(basisSize,1);   GS_FCI(1) = 1;
-    GS_CID          = zeros(basisSize-1,1); GS_CID(1) = 1;
+    GS_FCI          = [1 0 0 0 0 0];
+    GS_CID          = [1 0 0 0 0];
     f_FCI(k)        = V_FCI(1,1)^2;
     f_CID(k)        = V_CID(1,1)^2;
     
@@ -102,7 +98,7 @@ for k=1:n
                      gamma(4)^2*FCI(1,4) + gamma(5)^2*FCI(1,5));
     E3  = E3A + E3B;
     
-    e_RS3      = E0 + g*E1 + g^2*E2 + g^3*E3;
+    e_RS3      = E0 + g*E1;% + g^2*E2 + g^3*E3;
     E_RS3(k,1) = e_RS3;
 end
 
@@ -112,27 +108,45 @@ if plotting
     figure(1);
     legendA = cell(basisSize,1);
     for k=1:basisSize
-        plot(G, E_FCI(:,k));
+        if k==1
+            plot(G, E_FCI(:,k), 'b-', 'LineWidth', 2);
+        else 
+            if mod(k,2)==0
+                plot(G, E_FCI(:,k), '--');
+            else 
+                plot(G, E_FCI(:,k), '-');
+            end
+        end
         legendA{k} = sprintf('E%d', k);
         hold on;
     end
-    legend(legendA)
-    xlabel('g');
-    ylabel('E');
+    leg = legend(legendA);
+    set(leg, 'FontSize', 16, 'Interpreter', 'latex');
+    xlabel('g', 'FontSize', 16, 'Interpreter', 'latex');
+    ylabel('Energy(g)', 'FontSize', 16, 'Interpreter', 'latex');
     title('Full configuration interaction');
 
     % Energy plot (CID).
     figure(2);
     legendA = cell(basisSize-1,1);
     for k=1:basisSize-1
-        plot(G, E_CID(:,k));
+        if k==1
+            plot(G, E_CID(:,k), 'b-', 'LineWidth', 2);
+        else 
+            if mod(k,2)==0
+                plot(G, E_CID(:,k), '--');
+            else 
+                plot(G, E_CID(:,k), '-');
+            end
+        end
         legendA{k} = sprintf('E%d', k);
         hold on;
     end
-    legend(legendA)
-    xlabel('g');
-    ylabel('E');
-    title('Configuration interaction doubles');
+    leg = legend(legendA);
+    set(leg, 'FontSize', 16, 'Interpreter', 'latex');
+    xlabel('$g$', 'FontSize', 16, 'Interpreter', 'latex');
+    ylabel('Energy($g$)', 'FontSize', 16, 'Interpreter', 'latex');
+    %title('Configuration interaction doubles');
     
     % Energy plot (RS3).
     figure(3);
@@ -146,14 +160,31 @@ if plotting
     % state, |12> (index 1 under the mapping used here).
     figure(4);
     hold('on')
-    plot(G, f_FCI);
-    plot(G, f_CID);
-    xlabel('g');
-    ylabel('f_0');
-    legend('FCI','CID');
+    %plot(G, f_FCI, '-');
+    plot(G, f_CID, '--');
+    xlabel('$g$', 'FontSize', 16, 'Interpreter', 'latex');
+    ylabel('Probability of GS, $f_0$', 'FontSize', 16, 'Interpreter', 'latex');
+    leg = legend('CID');%,'CID');
+    set(leg, 'FontSize', 16, 'Interpreter', 'latex');
+    
+    figure(5);
+    plot(G, e_RS3, 'r-');
+    xlabel('g', 'FontSize', 16, 'Interpreter', 'latex');
+    ylabel('E0', 'FontSize', 16, 'Interpreter', 'latex');
+    leg = legend('RSPT3');
+    set(leg, 'FontSize', 16, 'Interpreter', 'latex');
+    
 end
 
-    
-    
+
+%%
+ax = gca;
+outerpos = ax.OuterPosition;
+ti = ax.TightInset; 
+left = outerpos(1) + ti(1);
+bottom = outerpos(2) + ti(2);
+ax_width = outerpos(3) - ti(1) - ti(3);
+ax_height = outerpos(4) - ti(2) - ti(4);
+ax.Position = [left bottom ax_width ax_height];    
     
     
